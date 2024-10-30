d4ed5f0ffc329481c42c8d8fcf0a2eb0c082927a
0xfffb <jailbreak@aliyun.com>
2024-10-30 18:29:10 +0800

    test

--- nest-client/src/main.rs
+++ nest-client/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, client!");
+use std::io;
+use std::net::SocketAddr;
+use tokio::io::{stdin, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+
+struct Servers {
+
+}
+
+#[tokio::main]
+async fn main() {
+    let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
+    let mut stream = TcpStream::connect(addr).await.unwrap();
+    let local_addr = stream.local_addr().unwrap();
+    println!("Local address: {}", local_addr);
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        stream.write(input.as_bytes()).await.unwrap();
+        // 读取服务器的响应
+        let mut buffer = [0; 1024];
+        let n = stream.read(&mut buffer).await.unwrap();
+        println!("Received from server: {:?}", &buffer[0..n]);
+    }
 }
--- nest-server/src/main.rs
+++ nest-server/src/main.rs
@@ -1,6 +1,60 @@
 
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
+
+
+struct Client {
+    addr: SocketAddr,
+    stream: Arc<Mutex<TcpStream>>,
+}
+
+impl Client {
+    pub fn new(addr: SocketAddr, stream: Arc<Mutex<TcpStream>>) -> Self {
+        Self { addr, stream }
+    }
+}
+
+struct StreamPool {
+    clients: HashMap<SocketAddr, Client>,
+}
+
+impl StreamPool {
+    fn new() -> Self { Self { clients: HashMap::new() } }
+
+    fn insert(&mut self, addr: SocketAddr, client: Client) {
+        self.clients.insert(addr, client);
+    }
+
+    fn get(&self, addr: SocketAddr) -> Option<&Client> { self.clients.get(&addr) }
+
+    fn remove(&mut self, addr: &SocketAddr) {
+        self.clients.remove(addr);
+    }
+
+    async fn handle(&mut self, addr: SocketAddr, mut stream: TcpStream) {
+        // let client = Client::new(addr, Arc::new(Mutex::new(stream)));
+        // self.clients.insert(addr, client);
+        let mut buffer = [0; 1024];
+        let n = stream.read(&mut buffer).await.unwrap();
+        println!("Receive: {:?}, from: {}", String::from_utf8_lossy(&buffer[..n]), addr);
+        stream.write(&buffer[0..n]).await.unwrap();
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, server!");
+
+    let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
+    let listener = TcpListener::bind(&addr).await.unwrap();
+    println!("listening on {}", addr);
+    let mut stream_pool = StreamPool::new();
+    loop {
+        let (mut stream, addr) = listener.accept().await.unwrap();
+        println!("Accepted connection from {}", addr);
+        tokio::spawn(&stream_pool.handle(addr, stream));
+    }
 }
